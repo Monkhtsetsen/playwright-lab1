a5f4bdce7bf8bdc2ea4e980c2b827aa65e087f07
Munkhtsetsen <munkhtsetsen.tumennasan@unimedia.mn~>
2026-09-02 15:59:37 +0800

    2 test nemsen

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -44,5 +44,54 @@ test.describe("sauce demo ui testuud: ", () => {
     //Sagsan deer 1 gesen too garch irseniig shalgah
     const Sags = page.locator(".shopping_cart_badge");
     await expect(Sags).toHaveText("1");
+
+    await page.getByRole("button", { name: "Open Menu" }).click();
+    const logoutLink = page.getByRole("link", { name: "Logout" });
+    await expect(logoutLink).toBeVisible();
+    await logoutLink.click();
+  });
+
+  //locked_out_user eer nevtreh
+  test("Locked out bolson usereer nevtreh", async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/");
+    await page.getByPlaceholder("Username").fill("locked_out_user");
+    await page.getByPlaceholder("Password").fill("secret_sauce");
+    await page.getByRole("button", { name: "Login" }).click();
+
+    //locked out bolson tuhai aldaanii message garch irehiig shalgah
+    const errorMSG = page.locator('[data-test="error"]');
+    await expect(errorMSG).toBeVisible();
+    await expect(errorMSG).toContainText(
+      "Epic sadface: Sorry, this user has been locked out.",
+    );
+  });
+  //Baraanii delgerengui page ruu orj harah
+  test("Baraanii ner deer darch delgerengui huudas ruu shiljih", async ({
+    page,
+  }) => {
+    await page.goto("https://www.saucedemo.com/");
+    await page.getByPlaceholder("Username").fill("standard_user");
+    await page.getByPlaceholder("Password").fill("secret_sauce");
+    await page.getByRole("button", { name: "Login" }).click();
+
+    //Ehnii baraanii ner deer darah
+    const firstProductName = page.locator(".inventory_item_name").first();
+    const productNameText = await firstProductName.textContent();
+    await firstProductName.click();
+
+    //Delgerengui huudasnii url bolon neriig batalgaajuulah
+    await expect(page).toHaveURL(/.*inventory-item.html/);
+    await expect(page.locator(".inventory_details_name")).toHaveText(
+      productNameText || "",
+    );
+
+    //Back to product tovchoor butsah
+    await page.getByRole("button", { name: "Back to products" }).click();
+    await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+
+    await page.getByRole("button", { name: "Open Menu" }).click();
+    const logoutLink = page.getByRole("link", { name: "Logout" });
+    await expect(logoutLink).toBeVisible();
+    await logoutLink.click();
   });
 });
